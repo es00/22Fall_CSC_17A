377aa1d2ae4ac8bed8264ff3e2e67089b1cc82ec
Elias Silva <73357334+es00@users.noreply.github.com>
2022-09-07 07:39:44 -0700

    Lab

---
+++ Lab/Intro Lab/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,39 @@
+
+
+/* 
+ * File:   main.cpp
+ * Author: Elias Silva
+ * Purpose: Hello World
+ *
+ * 
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) 
+{
+    //Initialize range Random Number Seed
+    
+    //Declare Variables
+   
+    //Initialize Variables
+    
+    //Map inputs to outputs -> The process
+
+    //Display Results
+   
+    cout << "Hello World" << endl;
+    
+    //Exit stage right
+    return 0;
+}
